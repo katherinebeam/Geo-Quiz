019434ce5a6f1ad212e105d2a24f07dc98ea39b1
Katherine Beam <k.beam@umiami.edu>
2016-06-05 15:52:06 -0400

    Initial Commit

---
+++ Geo-Quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Geo-Quiz
+//
+//  Created by xxxxxxx on 6/5/16.
+//  Copyright © 2016 Katherine Beam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
